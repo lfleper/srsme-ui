3c97564cd83e954be3f7bc74deadb23631b5ddb3
Lukas Fleper <lukasfleper@protonmail.ch>
2022-04-19 23:47:45 +0200

    export drawer

--- src/components/ExportDocumentDrawer.vue
+++ src/components/ExportDocumentDrawer.vue
@@ -4,17 +4,24 @@
         v-model="exportDocumentDrawerVisible"
     >
         <template #title>
-            Export {{doc.name}}
+            Export "{{doc.name}}"
         </template>
         <template #default>
-            <span>Test</span>
+            <el-select v-model="exportOption" placeholder="format" size="large">
+                <el-option
+                    v-for="option in exportOptions"
+                    :key="option.value"
+                    :label="option.label"
+                    :value="option.value"
+                />
+            </el-select>
         </template>
         <template #footer>
             <div style="flex: auto;">
                 <el-button @click="exportDocumentDrawerVisible = false">
                     Cancel
                 </el-button>
-                <el-button @click="exportDocument">
+                <el-button @click="exportDocument" type="primary">
                     Export
                 </el-button>
             </div>
@@ -25,20 +32,37 @@
 <script setup lang="ts">
 import {
     ElDrawer,
-    ElButton
+    ElButton,
+    ElSelect,
+    ElOption
 } from 'element-plus'
 import { FlatDocument } from '@/types'
-import { ref, toRefs, defineExpose, defineProps } from 'vue';
+import { ref, toRefs, defineExpose, defineProps, reactive } from 'vue';
 
 const props = defineProps<{
     doc: FlatDocument
 }>()
 const { doc } = toRefs(props)
-
 const exportDocumentDrawerVisible = ref(false)
+const exportOptions = reactive([
+    {
+        value: 'pdf',
+        label: 'PDF'
+    },
+    {
+        value: 'json',
+        label: 'JSON'
+    },
+    {
+        value: 'html',
+        label: 'HTML'
+    }
+])
+const exportOption = ref('')
+
 const open = () => exportDocumentDrawerVisible.value = true
 const exportDocument = () => {
-    console.log('Export document.')
+    console.log('export as ' + exportOption.value)
     exportDocumentDrawerVisible.value = false
 }
 
